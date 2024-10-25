e2da8bf4a2e526d2f15ef41d6ebea6373b74160f
Pedro Seriang <pedroseriangcorp@gmail.com>
2024-10-26 01:35:15 +0800

    Add files via upload

---
+++ asset.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Menghapus file 0 KB...
+forfiles /S /M *.* /C "cmd /c if @fsize==0 del @path"
+
+echo Menghapus folder kosong...
+for /f "delims=" %%d in ('dir /ad /b /s ^| sort /r') do rd "%%d"
+
+echo Menghapus semua file dari folder TEMP...
+del /q/f/s %TEMP%\*
+del /q/f/s C:\Windows\Temp\*
+
+echo Selesai!
+pause
